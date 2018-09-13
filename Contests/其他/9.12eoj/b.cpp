b59562f14b6c168f35c8975ecc943cf139937001
RandomVar <1035909127@qq.com>
2018-09-13 10:19:46 +0800

    commit

---
+++ Contests/其他/9.12eoj/b.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void dfs(int id,int pos,int val)
+{
+  /*
+  if(id==0)
+  {
+    if(pos==k)
+    {
+      ans=max(ans,val);
+    }
+    return;
+  }*/
+
+  /*int x=a[id+1].t-a[id].t;
+  for(int i=0;i<=x;i++)
+  {
+    dfs(id-1,pos-i,val+)
+  }*/
+}
+int main()
+{
+  int n,k,m;
+  while(scanf("%d%d%d",&n,&k,&m)==3)
+  {
+    for(int i=1;i<=n;i++)
+    {
+      scanf("%d%d%d",&a[i].a,&a[i].b,&a[i].t);
+    }
+    sort(a+1,a+1+n);
+    //  dfs(n, ,0);
+  }
+
+}
